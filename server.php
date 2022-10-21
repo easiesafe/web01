da0a0870bd265186af7a707b1df4b2e8751dd723
easiesafe <s.ptrp45.sth@gmail.com>
2022-10-21 16:50:01 +0700

    first time

---
+++ server.php
@@ -0,0 +1,15 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "register_db";
+
+// create Connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+// check connection
+if (!$conn) {
+    die("connection failed" .mysqli_connect_error());
+}
+?>
